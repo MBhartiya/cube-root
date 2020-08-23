aab69cdc2521c2c6d1ac64e5543c511e45af3473
Monica <mmonica@cisco.com>
2020-08-23 10:53:01 +0530

    adding login via GitHub

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,11 +8,15 @@ const options = {
       clientId: process.env.CLIENT_ID,
       clientSecret: process.env.CLINET_SECRET
     }),
+    Providers.GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET
+    })
     // ...add more providers here
-      Providers.Facebook({
-        clientId: process.env.FACEBOOK_CLIENT_ID,
-        clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-      })
+      // Providers.Facebook({
+      //   clientId: process.env.FACEBOOK_CLIENT_ID,
+      //   clientSecret: process.env.FACEBOOK_CLIENT_SECRET
+      // })
   ]
   // A database is optional, but required to persist accounts in a database
 //   database: process.env.DATABASE_URL,
